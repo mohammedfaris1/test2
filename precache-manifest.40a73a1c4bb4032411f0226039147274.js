986885f1541e33e732442c3cb7c20b59306bd37d
mohammedfaris1 <mohammedfarisofficial@gmail.com>
2020-07-11 13:36:43 +0530

    Updates

---
+++ precache-manifest.40a73a1c4bb4032411f0226039147274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5a3458c7bc2120b7052b970f2de79f",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "be8335b8d122014ba1e5",
+    "url": "/test2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f6c807064f62fa2012f",
+    "url": "/test2/static/js/2.d01e437f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test2/static/js/2.d01e437f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be8335b8d122014ba1e5",
+    "url": "/test2/static/js/main.99b563d5.chunk.js"
+  },
+  {
+    "revision": "64b17dba3a1e6860cbac",
+    "url": "/test2/static/js/runtime-main.70875b4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test2/static/media/logo.5d5d9eef.svg"
+  }
+]);
